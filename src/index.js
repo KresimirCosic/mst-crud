421d0013af5f5352333d08c5b2bdc605efa558ad
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2018-10-02 15:21:48 +0200

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Service Worker
+import * as serviceWorker from "./serviceWorker";
+// React
+import React from "react";
+// MobX React
+import { Provider } from "mobx-react";
+// React Router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// React DOM
+import ReactDOM from "react-dom";
+// Styles
+import "./index.css";
+// Pages
+import Home from "./pages/Home/Home";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppRoot = (
+  <Provider>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(AppRoot, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+// React
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
